7f6af2df2c331a6e3cb61f82e3078c17d7a3976a
Mikaela Miriam <mokwamikaela@gmail.com>
2025-05-04 01:58:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,177 @@
 const faqs = [
   {
+  categoria: "Mensagem de Apresentação",
+  texto: `Utilizar sempre que abrir os grupos
+
+Olá, meu nome é Mikaela, sou sua consultora Vitascience e estou aqui para te ajudar e tirar suas dúvidas sobre as ofertas da Campanha de Aniversário!
+
+Sejam todos bem-vindos. Desejo um dia abençoado para todos nós 🙏.
+Vamos lá?
+`
+  },
+  {
+  categoria: "Regras",
+  texto: `Regras do grupo
+
+
+Olá, gostaria da atenção de vocês para as regras de comportamento deste grupo.
+Ficam terminantemente *🚫PROIBIDAS* qualquer divulgação ou postagem (texto, vídeo, foto e links) referente a:
+🚫 *Qualquer produto/serviço para venda;*
+🚫*Links*
+🚫 *Qualquer tipo de ofensa, direta ou indireta, às pessoas que participam ou não deste grupo;*
+e qualquer tipo;*
+🚫*Compartilhamento de spam (Vídeos, imagens e textos que não sejam referentes a dúvidas da condição especial, dúvidas sobre os produtos Vitascience)*
+🚫 *Correntes de qualquer tipo;*
+🚫 *Que não estejam diretamente ligadas à finalidade do grupo;*
+⚠️ Quem insistir em descumprir as regras poderá ser EXCLUÍDO do grupo.`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 1
+
+Você já imaginou ter o acompanhamento do Dr. Dayan, a Equipe Vitascience, e vários outros profissionais da saúde como médicos, nutricionistas por um período de 1 ano*?
+
+Saiba que tudo isso é possível através do Programa de Acompanhamento Código da Vitalidade. 
+
+Ele é exclusivo para as pessoas que já passaram dos seus 50, 60, ou até mais de 70 anos, que queiram viver com autonomia, saúde, disposição, jovialidade e vitalidade.
+
+Então, o que você está esperando para fazer a sua inscrição e ter acesso a tudo isso e muito mais? 
+
+Fale com a nossa consultora (colocar o link da vendedora do grupo) e faça já a sua inscrição`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 2
+
+*Você sabia que ao se inscrever no Programa de Acompanhamento Código da Vitalidade terá acesso à*: 👇
+
+📲 Acompanhamento por 10 semanas no Whatsapp;
+📲 Acompanhamento por 1 ano (encontros TODA SEMANA)
+⭐ Acompanhamento de 1 ano com Dr. Dayan;
+🍎 Acompanhamento de 1 ano com Nutricionista;
+👥 Acompanhamento por 1 ano com Médicos e Especialistas Convidados;
+🗣 ️ Acompanhamento por 1 ano com a Equipe Vitascience.
+
+Além de…..
+
+📜Cardápios Anti-Idade e Anti problemas de saúde;
+🫕 Cozinha com Receitas (vídeo passo a passo);
+🧠 Curso: Intestino, Seu Segundo Cérebro;
+📕Livro Digital: Do Infarto ao Câncer: Como combater?
+📗Livro Digital: 101 Curas Naturais;
+📚Biblioteca da Jovialidade: Nossos melhores Livros Digitais de Saúde;
+🎁1x Pote do Melhor Multivitamínico, Multivita (Para quem se inscrever na segunda-feira até o meio-dia)
+🎁Sorteio de 10x Potes de Belavita (para os 100 primeiros inscritos)
+
+
+*Não perca mais tempo e faça agora mesmo a sua inscrição com a nossa consultora* (colocar o link da vendedora do grupo)`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 3
+
+*Se você está em busca de*:
+
+
+Energia e disposição: pra você aproveitar todos os momentos, todos os dias;
+
+Força física: pra você ter a capacidade de fazer as coisas que você mais gosta;
+
+Libido e potência: pra você reacender a paixão do seu relacionamento;
+
+Mente ativa: pra você manter sua lucidez, independência, capacidade de aprendizado e sempre lembrar dos momentos mais importantes da sua vida;
+
+Organismo forte: pra você se manter livre de doenças o máximo de tempo possível e, quando você tiver alguma, o seu corpo conseguir combater e se restaurar.
+
+O Programa de acompanhamento Código da Vitalidade foi feito para você. 
+
+O código da Vitalidade vai  proporcionar conhecimento e ferramentas que vão trazer vitalidade e saúde para sua vida, independente de como está sua saúde hoje o Programa de Acompanhamento vai mostrar tanto na teoria quanto na prática como desacelerar o envelhecimento do corpo, dando a você sabedoria para lidar com as marcas do tempo.
+
+*Faça agora mesmo a sua inscrição e tenha uma vida com muito mais saúde e vitalidade, alcançando a 3ª juventude após seus 50, 60, ou até mais de 70 anos com  a nossa consultora* (colocar o link da vendedora do grupo)`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 4
+
+Quem pode se beneficiar com o curso? 
+Ele é adequado para todas as idades e condições de saúde?
+
+O Código da Vitalidade foi projetado para atender a uma ampla variedade de pessoas, que desejam elevar seus níveis de bem-estar e compreensão sobre o próprio corpo.
+
+Todas as pessoas que buscam aprimorar sua saúde e vitalidade podem se beneficiar com o programa de acompanhamento. 
+
+Seja você um adulto jovem ou mais experiente, o Código da Vitalidade proporciona conhecimentos valiosos e estratégias práticas para melhorar a qualidade de vida.
+
+O Dr.Dayan Siebra, com sua expertise, apresenta os temas de forma clara e didática, tornando o curso adequado para todos que desejam cuidar melhor de si mesmo e de sua saúde.
+
+No geral, o "Código da Vitalidade" é destinado a pessoas que buscam uma jornada de aprendizado sobre o corpo humano e estratégias para melhorar a saúde física, mental e emocional. 
+
+*Não perca mais tempo e faça agora mesmo a sua inscrição com a nossa consultora* (colocar o link da vendedora do grupo)`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 5
+
+Você sabia que o Código da Vitalidade aborda questões específicas de envelhecimento e como você pode lidar com o processo de envelhecimento do corpo?
+
+O Dr. Dayan explora o funcionamento do corpo humano durante o processo de envelhecimento e apresenta estratégias práticas para lidar com esse momento inevitável da vida.
+
+Ao participar do curso, você terá a oportunidade de adquirir sabedoria sobre como o envelhecimento afeta o corpo e como é possível lidar com as mudanças que ocorrem ao longo do tempo. 
+
+O conteúdo aborda não apenas a parte física, mas também a mental e a emocional, visando proporcionar um envelhecimento mais saudável e pleno.
+
+Você aprenderá sobre a restauração do funcionamento das células do corpo e como melhorar a vitalidade em diferentes aspectos da sua vida. 
+
+O curso oferece conhecimentos valiosos sobre os sistemas fundamentais do corpo e como mantê-los saudáveis à medida que envelhecemos.
+
+*Faça agora mesmo a sua inscrição e tenha uma vida com muito mais saúde e vitalidade, alcançando a 3ª juventude após seus 50, 60, ou até mais de 70 anos com  a nossa consultora* (colocar o link da vendedora do grupo).`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 6
+
+É necessário ter algum conhecimento em saúde ou ciências do corpo para participar do Código da Vitalidade?
+
+Não precisa ter nenhum conhecimento em saúde ou ciências do corpo, para participar do curso "Código da Vitalidade". 
+
+O programa de acompanhamento foi elaborado de forma acessível e didática, visando atender a todos os participantes, independentemente do seu nível de conhecimento ou experiência na área.
+
+O Dr. Dayan aborda os temas de forma clara e compreensível, tornando o conteúdo facilmente assimilável por todos. 
+
+Ele guiará os alunos em uma jornada de aprendizado sobre o funcionamento do corpo humano e estratégias práticas para melhorar a saúde e vitalidade.
+
+Seja você leigo ou com conhecimentos básicos sobre saúde, o programa oferecerá informações valiosas que contribuirão para uma maior compreensão do seu próprio corpo e suas necessidades.
+
+O Código da Vitalidade é uma oportunidade para todos se aprofundarem e aprenderem com conhecimentos claros em uma linguagem simples, buscando uma vida mais saudável e equilibrada.
+
+Fale com a nossa consultora (colocar o link da vendedora do grupo) e faça já a sua inscrição`
+  },
+  {
+  categoria: "Mensagens para interação CÓD",
+  texto: `Mensagem 7
+
+🔥Um time com especialistas de renome para estar com você durante 1 ano.
+
+Conheça todos os especialistas: 
+
+✔  Dr. Dayan Siebra: cirurgião vascular, especialista em saúde natural, emagrecimento e longevidade;
+✔ Poliane Cardoso - Maior referência em ozonioterapia na estética e no combate à doença.
+✔ Samuel Dalle Laste - Médico clínico com práticas em medicina ortomolecular;
+✔ Sonia Umbelino - Médica especialista em menopausa;
+✔ Kelly Lemos - Fisioterapeuta especialista em combate à dor crônica;
+✔ Conceição Trucom - Vai te mostrar como digerir, regenerar e relaxar para uma vida muito melhor;
+✔ Denise Carreiro - Nutricionista clínica, autora de 19 livros sobre nutrição e Docente VP;
+✔ E muito mais! (são mais de 13 especialistas confirmados para te acompanhar durante esse 1x ano de Código da Vitalidade.;
+
+Aproveite esta oportunidade e garanta sua vaga agora:`
+  },
+  {
+  categoria: "Tabelas nutricionais dos suplementos",
+  texto: `PROSTENVITA 
+
+src="https://drive.google.com/file/d/1Ud9yLpY6_xHW5wgmy8C0vkpFTJOV92nv/view?usp=drivesdk"`
+  },
+  {
     categoria: "Parabéns",
     texto: `Parabéns pela sua conquista!!! 🎉
 Você deu um importante passo para ter uma vida com mais saúde e vitalidade.`
